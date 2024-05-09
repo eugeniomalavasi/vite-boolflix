ce96ae18c8d1ccc427d360559e825bc0ef0d9b4b
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-09 18:20:40 +0200

    feat: flip card inserted

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -32,18 +32,89 @@ export default {
 
 <template>
     <div class="card">
-        <img :src="getPosterPath(card.poster_path)" class="poster" alt="">
-        <h3>{{ card.title }}</h3>
-        <h3>{{ card.original_name }}</h3>
-        <img class="flag" :src="getImageUrl(card.original_language)" alt="">
-        <i class="fa-solid fa-star" v-for="num in roundNumb(card.vote_average)"></i>
-        <i class="fa-regular fa-star" v-for="num in 5 - roundNumb(card.vote_average)"></i>
+        <div class="inner-card">
+
+            <div class="card-front">
+                <img :src="getPosterPath(card.poster_path)" class="poster" alt="">
+            </div>
+
+            <div class="card-back">
+                <h3>{{ card.title }}</h3>
+                <h3>{{ card.original_name }}</h3>
+                <p>{{ card.overview }}</p>
+                <img class="flag" :src="getImageUrl(card.original_language)" alt="">
+                <div>
+                    <i class="fa-solid fa-star fullstar" v-for="num in roundNumb(card.vote_average)"></i>
+                    <i class="fa-regular fa-star fullstar" v-for="num in 5 - roundNumb(card.vote_average)"></i>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
-        .flag {
-            width: 30px;
+.card {
+    background-color: transparent;
+    height: 513px;
+    width: 342px;
+    perspective: 1000px;
+
+    &:hover .inner-card {
+        transform: rotateY(180deg);
+    }
+
+    .inner-card {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform 0.8s;
+        transform-style: preserve-3d;
+
+        .card-front,
+        .card-back {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            -webkit-backface-visibility: hidden;
+            backface-visibility: hidden;
+
+            .fullstar {
+                color: orange;
+            }
         }
 
+        .card-front {
+            background-color: #bbb;
+            color: black;
+        }
+
+        .card-back {
+            background-color: lightgray;
+            color: white;
+            transform: rotateY(180deg);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            gap: 1rem;
+            padding: 1rem;
+            h3 {
+                color: orange;
+                font-size: 2rem;
+            }
+            p {
+                font-size: 0.8rem;
+                color: black;
+            }
+        }
+    }
+
+
+}
+
+.flag {
+    width: 30px;
+}
 </style>
