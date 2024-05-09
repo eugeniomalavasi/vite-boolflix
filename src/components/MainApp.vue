9b4b317aad00b445b232a62f7e331f77ff5f77f6
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-05-09 13:00:25 +0200

    feat: star are printig

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -14,7 +14,13 @@ export default {
                 return new URL(`../assets/rest.png`, import.meta.url).href;
             }
         }, getPosterPath(posterImg) {
-            return new URL(`${this.posterUrl}${posterImg}`, import.meta.url).href;
+            if (posterImg != null) {
+                return new URL(`${this.posterUrl}${posterImg}`, import.meta.url).href;
+            } else {
+                return new URL("../assets/img-not-found.jpg", import.meta.url).href;
+            }
+        }, roundNumb(fractNumb) {
+            return Math.ceil((fractNumb) / 2);
         }
     }
 }
@@ -25,20 +31,20 @@ export default {
         <div class="row">
             <div class="col" v-for="(movieCard, i) in store.moviesArray">
                 <div class="card">
-                    <img :src="getPosterPath(movieCard.poster_path)" alt="">
-                    <!-- <img :src="posterUrl + movieCard.poster_path" alt=""> -->
+                    <img :src="getPosterPath(movieCard.poster_path)" class="poster" alt="">
                     <h3>{{ movieCard.title }}</h3>
                     <h3>original name: {{ movieCard.original_name }}</h3>
-                    <p>vote: {{ movieCard.vote_average }}</p>
                     <img class="flag" :src="getImageUrl(movieCard.original_language)" alt="">
+                    <i class="fa-solid fa-star" v-for="num in roundNumb(movieCard.vote_average)"></i>
                 </div>
             </div>
             <div class="col" v-for="(seriesCard, i) in store.seriesArray">
                 <div class="card">
+                    <img :src="getPosterPath(seriesCard.poster_path)" alt="">
                     <h3>title: {{ seriesCard.name }}</h3>
                     <h3>original name: {{ seriesCard.original_name }}</h3>
-                    <p>vote: {{ seriesCard.vote_average }}</p>
                     <img class="flag" :src="getImageUrl(seriesCard.original_language)" alt="">
+                    <i class="fa-solid fa-star" v-for="num in roundNumb(seriesCard.vote_average)"></i>
                 </div>
             </div>
         </div>
@@ -46,8 +52,18 @@ export default {
 </template>
 
 <style lang="scss" scoped>
-.flag {
-    width: 30px;
-    max-width: 100%;
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+
+    .col {
+        width: 33%;
+
+        .flag {
+            width: 30px;
+        }
+    }
+
 }
 </style>
